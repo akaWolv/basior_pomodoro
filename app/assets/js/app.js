229248ad1d9f0e313275b5c5888e22abad90be66
Wolv <akaWolv@gmail.com>
2015-06-22 02:27:39 +0200

    Initial

---
+++ app/assets/js/app.js
@@ -0,0 +1,359 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+var pomodoroApp = angular.module('PomodoroApp', []);
+
+pomodoroApp.factory('socket', function ($rootScope, $location) {
+    if ('undefined' === typeof io)
+    {
+        return false;
+    }
+
+    var socket = io.connect($location.host() + ':3001');
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
+
+pomodoroApp.filter('secondsToDateTime', [function() {
+    return function(seconds) {
+        return new Date(1970, 0, 1).setSeconds(seconds);
+    };
+}]);
+
+pomodoroApp.controller('MainCtrl', function($scope, $rootScope, $location, socket) {
+    $rootScope.ConnectionData = {
+        channel_name : undefined,
+        user_email : undefined,
+        user_name : undefined
+    }
+
+    $scope.ConnectionForm = {
+        channel_name : undefined,
+        user_email : undefined,
+        user_name : undefined
+    }
+
+    $scope.$current_path = function(){
+        var path = $location.path.call($location);
+        var path_splited = path.split('/');
+        var result = [];
+        for(var k in path_splited) {
+            if (0 < path_splited[k].length) {
+                result.push(path_splited[k]);
+            }
+        }
+        return result;
+    };
+
+    $scope.$updateUrl = function() {
+        var url_parts = [];
+
+        if (angular.isDefined($scope.ConnectionData.channel_name)) {
+            url_parts.push($scope.ConnectionData.channel_name);
+            if (angular.isDefined($scope.ConnectionData.user_email)) {
+                url_parts.push($scope.ConnectionData.user_email);
+                if (angular.isDefined($scope.ConnectionData.user_name)) {
+                    url_parts.push($scope.ConnectionData.user_name);
+                }
+            }
+        }
+
+        var url = url_parts.join('/');
+
+        if (url.length > 0)
+        {
+            $location.path('/' + url);
+        }
+    }
+
+    $scope.updateConnection = function() {
+
+        if (angular.isDefined($scope.ConnectionForm.channel_name) && 0 < $scope.ConnectionForm.channel_name.length) {
+            $rootScope.ConnectionData.channel_name = $scope.ConnectionForm.channel_name;
+            if (angular.isDefined($scope.ConnectionForm.user_email) && 0 < $scope.ConnectionForm.user_email.length) {
+                $rootScope.ConnectionData.user_email = $scope.ConnectionForm.user_email;
+                if (angular.isDefined($scope.ConnectionForm.user_name) && 0 < $scope.ConnectionForm.user_name.length) {
+                    $rootScope.ConnectionData.user_name = $scope.ConnectionForm.user_name;
+                }
+                else {
+                    $rootScope.ConnectionData.user_name = undefined;
+                }
+            }
+            else {
+                $rootScope.ConnectionData.user_email = undefined;
+                $rootScope.ConnectionData.user_name = undefined;
+            }
+        }
+        else {
+            $rootScope.ConnectionData.channel_name = undefined;
+            $rootScope.ConnectionData.user_email = undefined;
+            $rootScope.ConnectionData.user_name = undefined;
+        }
+
+        $scope.$updateUrl();
+    }
+
+    $scope.$formValuesFromUrl = function() {
+        var url_details = $scope.$current_path();
+
+        $scope.ConnectionForm.channel_name = angular.isDefined(url_details[0]) ? url_details[0] : undefined;
+        $scope.ConnectionForm.user_email = angular.isDefined(url_details[1]) ? url_details[1] : undefined;
+        $scope.ConnectionForm.user_name = angular.isDefined(url_details[2]) ? url_details[2] : undefined;
+        $scope.updateConnection();
+    }
+    $scope.$formValuesFromUrl();
+
+
+    socket.on('sync time', function(time) {
+        console.log( time );
+    });
+});
+
+pomodoroApp.controller('ChannelCtrl', function($scope, $rootScope, socket) {
+    $scope.channel = {};
+    $scope.pomodores = {};
+
+    $rootScope.$watchCollection('ConnectionData.channel_name', function(newValue){
+        if (undefined != newValue) {
+            $scope.channel.name = newValue;
+            socket.emit('pick pipe', {pipe : newValue});
+        }
+    });
+
+    socket.on('users list', function(pomodores) {
+        console.log(pomodores);
+        $scope.pomodores = pomodores;
+    });
+});
+
+pomodoroApp.controller('PomodorCtrl', function($scope, $rootScope, socket, $interval) {
+    //$scope.pomodor = {
+    //    current: 0,
+    //    email: "",
+    //    interval: "",
+    //    md5_hash: "",
+    //    name: "",
+    //    state: "",
+    //    started_on: "",
+    //    current_tme: ""
+    //};
+
+    $scope.init = function(pomodor) {
+        $scope.pomodor = pomodor;
+        $scope.updateTimer();
+
+        // current user
+        if ($scope.pomodor.email == $scope.ConnectionData.user_email) {
+            $rootScope.current_user = $scope.pomodor;
+        }
+    }
+
+    $scope.tick = function(){
+        if ('running' == $scope.pomodor.state) {
+            if (0 >= $scope.pomodor.current) {
+                if ($scope.pomodor.email == $scope.ConnectionData.user_email) {
+                    $rootScope.timerUp = true;
+                }
+            }
+            else {
+                $scope.pomodor.current--;
+            }
+        }
+    }
+    $scope.counter = $interval(function(){ $scope.tick(); }, 1000);
+
+    socket.on('user update ' + $scope.pomodor.email, function(details){
+        $scope.pomodor = details;
+    });
+
+    $scope.pomodorClass = function() {
+        var name = '';
+        switch ($scope.pomodor.state)
+        {
+            case 'running':
+                switch ($scope.pomodor.interval)
+                {
+                    case '5':
+                        name = 'colors-interval-5'
+                        break;
+                    case '25':
+                        name = 'colors-interval-25'
+                        break;
+                    default:
+                        name = 'colors-interval-break'
+                        break;
+                }
+                break;
+            case 'stopped':
+                name = 'colors-interval-break'
+                break;
+            case 'paused':
+                name = 'colors-interval-pause'
+                break;
+        }
+        return name;
+    }
+
+    $scope.pomodorText = function() {
+        var text = '';
+        switch ($scope.pomodor.state)
+        {
+            case 'running':
+                switch ($scope.pomodor.interval)
+                {
+                    case '5':
+                        text = 'minor tasks'
+                        break;
+                    case '25':
+                        text = 'do not disturb'
+                        break;
+                    default:
+                        text = 'waiting'
+                        break;
+                }
+                break;
+            case 'stopped':
+                text = 'waiting'
+                break;
+            case 'paused':
+                text = 'paused'
+                break;
+        }
+        return text;
+    }
+
+    $scope.updateTimer = function() {
+        var date1 = new Date($scope.pomodor.started_on);
+        var date2 = new Date($scope.pomodor.current_time);
+
+        var diff = date2 - date1;
+        var diffSeconds = diff / 1000;
+        if (NaN != diffSeconds && 0 < $scope.pomodor.current - diffSeconds) {
+            $scope.pomodor.current -= diffSeconds;
+        }
+        else {
+            $scope.pomodor.current = 0;
+            $scope.pomodor.state = 'stopped';
+        }
+    }
+});
+
+pomodoroApp.controller('UserCtrl', function($scope, $rootScope, socket) {
+    //$scope.user = {
+    //    name : undefined,
+    //    email : undefined,
+    //    current : undefined,
+    //    interval : undefined,
+    //    state : 'stopped',
+    //    md5_hash : undefined
+    //};
+    $scope.user = {};
+    $scope.paused = false;
+
+    $rootScope.$watch('timerUp', function(newValue){
+        if (true === newValue)
+        {
+            $scope.stopTimer();
+            switch ($scope.user.interval) {
+                case '25':
+                    if (confirm('Interval 25\' passed. Switch to interval 5\'?')) {
+                        $scope.startTimer('5', 300);
+                    }
+                    break;
+                case '5':
+                    if (confirm('Interval 5\' passed. Switch to interval 25\'?')) {
+                        $scope.startTimer('25', 1500);
+                    }
+                    break;
+            }
+        }
+        $rootScope.timerUp = undefined;
+    });
+
+    $rootScope.$watchCollection('ConnectionData', function(){
+        $scope.user.name = $rootScope.ConnectionData.user_name;
+        $scope.user.email = $rootScope.ConnectionData.user_email;
+
+        if (angular.isDefined($scope.user.email) && angular.isUndefined($scope.user.name)) {
+            var splited = $scope.user.email.split('@');
+            $scope.user.name = splited[0];
+        }
+
+        if (angular.isDefined($scope.user.email) && angular.isDefined($scope.user.name)) {
+            $scope.user.md5_hash = calcMD5($scope.user.email);
+            socket.emit('connect user', $scope.user);
+        }
+
+
+        socket.on('user update ' + $scope.user.email, function(details){
+            $scope.user = details;
+        });
+    });
+
+    $rootScope.$watchCollection('current_user', function(newValue){
+        if (undefined != newValue)
+        {
+            $scope.user.current = newValue.current;
+            $scope.user.interval = newValue.interval;
+            $scope.user.state = newValue.state;
+            $scope.user.md5_hash = newValue.md5_hash;
+
+            socket.emit('change timer', {state : $scope.user.state, interval : $scope.user.interval, seconds_left : $scope.user.current});
+        }
+    });
+
+    $scope.startTimer = function(interval, secondsLeft) {
+        $scope.paused = false;
+        socket.emit('change timer', {state : 'running', interval : interval, seconds_left : secondsLeft});
+    }
+
+    $scope.pauseTimer = function() {
+        $scope.paused = !$scope.paused
+        if (true === $scope.paused) {
+            socket.emit('change timer', {state : 'paused', interval : $scope.user.interval, seconds_left : $scope.user.current});
+        }
+        else {
+            socket.emit('change timer', {state : 'running', interval : $scope.user.interval, seconds_left : $scope.user.current});
+        }
+    }
+
+    $scope.stopTimer = function() {
+        $scope.paused = false;
+        socket.emit('change timer', {state : 'stopped', interval : $scope.user.interval, seconds_left : 0});
+    }
+
+    $scope.actions = [
+        {action : 'Start 25', call_func : '$scope.startTimer(\'25\', 1500)', img_class : 'glyphicon-play', text_class : 'colors-interval-25'},
+        {action : 'Start 5', call_func : '$scope.startTimer(\'5\', 300)', img_class : 'glyphicon-play', text_class : 'colors-interval-5'},
+        {action : 'Pause', call_func : '$scope.pauseTimer()', img_class : 'glyphicon-pause', text_class : 'colors-interval-pause'},
+        {action : 'Stop', call_func : '$scope.stopTimer()', img_class : 'glyphicon-stop', text_class : 'colors-interval-break'}
+    ];
+    $scope.actions_intervals = [
+        {action : 'Restart 20/25', call_func : '$scope.startTimer(\'25\', 1200)', img_class : 'glyphicon-play', text_class : 'colors-interval-25'},
+        {action : 'Restart 15/25', call_func : '$scope.startTimer(\'25\', 900)', img_class : 'glyphicon-play', text_class : 'colors-interval-25'},
+        {action : 'Restart 10/25', call_func : '$scope.startTimer(\'25\', 600)', img_class : 'glyphicon-play', text_class : 'colors-interval-25'},
+        {action : 'Restart 5/25', call_func : '$scope.startTimer(\'25\', 300)', img_class : 'glyphicon-play', text_class : 'colors-interval-25'}
+    ];
+
+    $scope.call = function(func) {
+        eval(func);
+    }
+});
---
+++ nodejs/server.js
@@ -0,0 +1,155 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+// rooms which are currently available in chat
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+var channels_users = {};
+var disconnect_timers = {}
+
+io.on('connection', function(socket){
+    socket.user = undefined;
+    socket.picked_pipe = undefined;
+
+    socket.on('pick pipe', function(cmd){
+        stopDisconnectTimer();
+
+        socket.join(cmd.pipe);
+        socket.picked_pipe = cmd.pipe;
+
+        if (undefined == channels_users[socket.picked_pipe]) {
+            channels_users[socket.picked_pipe] = {};
+        }
+
+        registerUserToPipe();
+
+        emitUsersListInChannel();
+
+        infoLog('PIPE PICKED', {'connected_to' : socket.picked_pipe});
+    });
+
+    socket.on('connect user', function(details){
+        stopDisconnectTimer();
+
+        if (undefined != details && undefined != details.email)
+        {
+            // jezeli user podal juz dane
+            unregisterUserFromPipe();
+
+            var alreadyConnected = false;
+            if (socket.user) {
+                alreadyConnected = true;
+            }
+
+            socket.user = details;
+
+            registerUserToPipe();
+
+            if (alreadyConnected) {
+                io.sockets.in(socket.picked_pipe).emit('user update ' + socket.user.email, socket.user);
+            }
+            else {
+                emitUsersListInChannel();
+            }
+
+            infoLog('USER CONNECTED', details);
+        }
+    });
+
+    socket.on('change timer', function(details){
+        stopDisconnectTimer();
+
+        if (undefined == socket.user)
+        {
+            return false;
+        }
+
+        socket.user.interval = details.interval;
+        socket.user.current = details.seconds_left;
+        socket.user.state = details.state;
+
+        channels_users[socket.picked_pipe][socket.user.email] = socket.user;
+        channels_users[socket.picked_pipe][socket.user.email].started_on = socket.handshake.time;
+
+        io.sockets.in(socket.picked_pipe).emit('user update ' + socket.user.email, socket.user);
+
+        infoLog('CHANGE TIMER', details);
+    });
+
+
+    var registerUserToPipe = function() {
+        if (undefined != socket.picked_pipe && undefined != socket.user && undefined != socket.user.email) {
+            channels_users[socket.picked_pipe][socket.user.email] = socket.user;
+
+            infoLog('USER REGISTERED TO PIPE', {pipe : socket.picked_pipe, user : socket.user.email});
+        }
+    }
+
+    var unregisterUserFromPipe = function() {
+        if (undefined != socket.picked_pipe && undefined != socket.user && undefined != socket.user.email) {
+            delete channels_users[socket.picked_pipe][socket.user.email];
+        }
+    }
+
+    var addCurrentTimer = function() {
+        if (undefined != socket.picked_pipe) {
+            for (var k in channels_users[socket.picked_pipe]) {
+                channels_users[socket.picked_pipe][k].current_time = socket.handshake.time;
+            }
+        }
+    }
+
+    var emitUsersListInChannel = function() {
+        if (undefined != socket.picked_pipe) {
+            addCurrentTimer();
+
+            io.sockets.in(socket.picked_pipe).emit('users list', channels_users[socket.picked_pipe]);
+        }
+    }
+
+    var infoLog = function(log_name, log) {
+        var info_log = {};
+        info_log.time = socket.handshake.time;
+        info_log.pipe = socket.picked_pipe;
+        info_log.client_id = socket.client.id;
+        info_log.ip = socket.conn.remoteAddress;
+        // merge two objects
+        if (undefined != log && 'object' === typeof log) {
+            for (var attrname in log) {
+                info_log[attrname] = log[attrname];
+            }
+        }
+        console.log(log_name + '|' + JSON.stringify(info_log));
+    }
+
+    var stopDisconnectTimer = function() {
+        if (undefined != socket.user && undefined != socket.user.email && undefined != disconnect_timers[socket.user.email]) {
+            clearInterval(disconnect_timers[socket.user.email]);
+            infoLog('DISCONNECT TIMER STOPPED', {email : socket.user.email});
+        }
+    }
+
+    socket.on('disconnect', function() {
+        if (undefined != socket.user && undefined != socket.user.email) {
+
+            infoLog('DISCONNECT TIMER STARTED 5\'', {email : socket.user.email});
+            disconnect_timers[socket.user.email] = setInterval(function(){
+                unregisterUserFromPipe();
+                emitUsersListInChannel();
+                infoLog('DISCONNECT', {email : socket.user.email});
+                clearInterval(disconnect_timers[socket.user.email]);
+                delete disconnect_timers[socket.user.email];
+            }, 1800000);
+        }
+    });
+
+    infoLog('JOIN');
+});
+
+
+http.listen(3001, function(){
+    console.log('listening on *:3001');
+});
